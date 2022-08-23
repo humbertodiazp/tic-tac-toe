2b24a781ec74f19d3eea0563657f5988ad13e4be
Humberto <hdkaratechop@gmail.com>
2022-08-22 18:55:29 -0700

    Add array to map with tic_tac_toe board.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -12,22 +12,26 @@ class Board
     # then you can simply change the value of the array at a gien point to represent to x or o for tic/tac/toe
 
     def game_board
+        player_answer = []
         x = 'x'
         o = 'o'
 
-        puts "#{x}  |  #{o}  |  #{x}"
+        puts "#{x_o[0]}  |  #{x_o[1]}  |  #{x_o[2]}"
         puts "_______________"
-        puts "#{o}  |  #{x}  |  #{o}" 
+        puts "#{x_o[3]}  |  #{x_o[4]}  |  #{x_o[5]}" 
         puts "_______________"
-        puts "#{o}  |  #{x}  |  #{x}" 
+        puts "#{x_o[6]}  |  #{x_o[7]}  |  #{x_o[8]}" 
         # puts "$#{player_answer}|$#{player_answer}|$#{player_answer}
         #  $#{player_answer}|$#{player_answer}|$#{player_answer}"
     end
+
+    x_o = []
     
 end
 
 def start_game
-    "Get ready for tic-tac-toe. Player 1 enter your selection."
+    puts "Get ready for tic-tac-toe. Player 1 enter your selection."
+    player_answer = gets.chomp 
 end
 
 class Game(player_answer) < Board
