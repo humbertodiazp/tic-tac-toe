b6cc6ebf088dd1975fab7cc15987b5c2314820aa
Humberto <hdkaratechop@gmail.com>
2022-08-25 00:12:49 -0700

    Require tic_tac_toe for test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,16 +1,17 @@
-require './tic-tac-toe' 
+#spec/tic_tac_toe_spec.rb
+require './tic_tac_toe.rb' 
 
 describe Board do
     describe "#game_board" do
       it "returns a tic-tac-toe board with numbers 1-9" do
         board = Board.new
-        expect(board.game_board).to eql(
-          puts "#{player_options[0]}  |  #{player_options[1]}  |  #{player_options[2]}"
+        expect(board.game_board(1, player_1)).to eql(
+          puts "X |  #{player_options[1]}  |  #{player_options[2]}"
           puts "_______________"
           puts "#{player_options[3]}  |  #{player_options[4]}  |  #{player_options[5]}" 
           puts "_______________"
           puts "#{player_options[6]}  |  #{player_options[7]}  |  #{player_options[8]}" 
         )
       end
-    end
   end
+end
