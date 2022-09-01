d7ffeb6c2164e6843e29727530aa952b11c78b0f
Humberto <hdkaratechop@gmail.com>
2022-08-31 20:49:57 -0700

    Remove arguments from game_board--was causing argument error.

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,7 +5,12 @@ describe Board do
     describe "#next_turn" do
       it "returns a tic-tac-toe board with player moves" do
         board = Board.new()
-        expect(board.next_turn(3, 'x')).to eq(puts board.game_board(3, 'x'))
+        expect(board.next_turn(3, 'x')).to eq(puts 
+          "#{squares[0]}  |  #{squares[1]}  |  #{squares[2] = 'x'}"
+          puts "_______________"
+          puts "#{squares[3]}  |  #{squares[4]}  |  #{squares[5]}" 
+          puts "_______________"
+          puts "#{squares[6]}  |  #{squares[7]}  |  #{squares[8]}" )
       end
   end
 end
