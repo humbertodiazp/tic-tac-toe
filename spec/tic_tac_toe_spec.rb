e09b83c4ec52a07b274aa22ae5e5a38f62a90439
Humberto <hdkaratechop@gmail.com>
2022-09-01 22:16:19 -0700

    Update test criteria, change paramenters. Test not working, but closer.

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,7 +5,9 @@ describe Board do
     describe "#next_turn" do
       it "returns a tic-tac-toe board with player moves" do
         board = Board.new()
-        expect(board.next_turn(1, 'x')).to eq(board.game_board())
+        actual = board.next_turn(1, 'x')
+        expected = board.game_board()
+        expect(actual).to eq expected
       end
 
       it "inserts player move at specified index in @squares array" do
@@ -16,8 +18,8 @@ describe Board do
       describe "#game_over" do
         it "declares a winner" do
           board = Board.new()
-          
         end
+    end
   end
 end
 
